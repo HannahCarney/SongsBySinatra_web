6473629e71fbe60bd31c8e890c08fbd9ac6a2511
Hannah Carney <hannahcarneyart@gmail.com>
2015-01-13 13:27:03 +0000

    removed some repititon

--- main.rb
+++ main.rb
@@ -96,25 +96,6 @@ class Test < Sinatra::Application
 
   not_found do
     slim :not_found
-  end
-
-  get '/login' do
-    slim :login
-  end
-
-  post '/login' do
-    if params[:username] == settings.username && params[:password] == settings.password
-      session[:admin] = true
-      redirect to('/songs')
-    else
-      slim :login
-   end
-  end
-
-  get '/logout' do
-    session.clear
-    redirect to ('/login')
-  end
- 
+  end 
 end
 
